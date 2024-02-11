995d3fe153faabc0409a0b9c95ea8b37cbe8e085
vrudas <vrudas4work@gmail.com>
2024-02-11 20:39:22 +0200

    Implemented code generation from openapi.yaml

---
+++ practice/open-api-generation/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    java
+    idea
+    id("org.openapi.generator") version "7.2.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+val javaxVersion = "1.3.2"
+val jsr305Version = "3.0.2"
+val gsonVersion = "2.10.1"
+
+dependencies {
+    implementation("javax.annotation:javax.annotation-api:$javaxVersion")
+    implementation("com.google.code.findbugs:jsr305:$jsr305Version")
+    implementation("com.google.code.gson:gson:$gsonVersion")
+}
+
+openApiGenerate {
+    generatorName = "java"
+    inputSpec = layout.projectDirectory.file("src/main/resources/openapi.yaml").asFile.path
+    outputDir = layout.projectDirectory.asFile.path
+
+    modelPackage.set("dev.vrudas.glp.ciphers.models")
+
+    generateModelTests.set(false)
+    generateModelDocumentation.set(false)
+
+    globalProperties.set(mapOf(
+        "apis" to "false",
+        "invokers" to "false",
+        "models" to ""
+    ))
+}
+
+tasks.compileJava {
+    dependsOn(tasks.openApiGenerate)
+}
